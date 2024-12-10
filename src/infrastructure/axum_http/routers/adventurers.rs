dcc973c1ebf02d263a753a70d6c94ad35f6ee747
NiebAnupat <anupat.k64@rsu.ac.th>
2024-12-10 23:05:22 +0700

    feat : add route (not done)

---
+++ src/infrastructure/axum_http/routers/adventurers.rs
@@ -0,0 +1,30 @@
+﻿use std::sync::Arc;
+use axum::extract::State;
+use axum::response::IntoResponse;
+use axum::{Json, Router};
+use axum::routing::post;
+use crate::application::usecases::adventurers::AdventurersUseCase;
+use crate::domain::repositories::adventurers::AdventurersRepository;
+use crate::domain::value_objects::adventurer_model::RegisterAdventurerModel;
+use crate::infrastructure::postgres::postgres_connection::PgPoolSquad;
+use crate::infrastructure::postgres::repositories::adventurers::AdventurersPostgres;
+
+pub fn routes(db_pool: Arc<PgPoolSquad>) -> Router {
+    // repository -> use case -> router
+    let adventurers_repository = AdventurersPostgres::new(db_pool);
+    let adventurers_use_case = AdventurersUseCase::new(Arc::new(adventurers_repository));
+
+    Router::new()
+        .route("/", post(register))
+        .with_state(Arc::new(adventurers_use_case))
+}
+
+pub async fn register<T>(
+    State(adventurers_use_case): State<Arc<AdventurersUseCase<T>>>
+    , Json(register_adventurer_model): Json<RegisterAdventurerModel>)
+    -> impl IntoResponse
+where
+    T: AdventurersRepository + Send + Sync,
+{
+    unimplemented!()
+}
---
+++ src/infrastructure/axum_http/routers/crew_switchboard.rs
@@ -0,0 +1,48 @@
+﻿use crate::application::usecases::crew_switchboard::CrewSwitchboardUseCase;
+use crate::domain::repositories::crew_switchboard::CrewSwitchboardRepository;
+use crate::domain::repositories::quest_viewing::QuestViewingRepository;
+use crate::infrastructure::postgres::postgres_connection::PgPoolSquad;
+use crate::infrastructure::postgres::repositories::crew_switchboard::CrewSwitchboardPostgres;
+use crate::infrastructure::postgres::repositories::quest_viewing::QuestViewingPostgres;
+use axum::extract::{Path, State};
+use axum::response::IntoResponse;
+use axum::routing::{delete, post};
+use axum::{Extension, Router};
+use std::sync::Arc;
+
+pub fn routes(db_pool: Arc<PgPoolSquad>) -> Router {
+    // repository -> use case -> router
+    let crew_switchboard_repository = CrewSwitchboardPostgres::new(Arc::clone(&db_pool));
+    let quest_viewing_repository = QuestViewingPostgres::new(Arc::clone(&db_pool));
+    let crew_switchboard_use_case = CrewSwitchboardUseCase::new(Arc::new(crew_switchboard_repository), Arc::new(quest_viewing_repository));
+
+    Router::new()
+        .route("/join", post(join))
+        .route("/leave", delete(leave))
+        .with_state(Arc::new(crew_switchboard_use_case))
+}
+
+pub async fn join<T1, T2>(
+    State(crew_switchboard_use_case): State<Arc<CrewSwitchboardUseCase<T1, T2>>>,
+    Path(quest_id): Path<i32>,
+    Extension(adventurer_id): Extension<i32>)
+    -> impl IntoResponse
+where
+    T1: CrewSwitchboardRepository + Send + Sync,
+    T2: QuestViewingRepository + Send + Sync,
+{
+    unimplemented!()
+}
+
+pub async fn leave<T1, T2>(
+    State(crew_switchboard_use_case): State<Arc<CrewSwitchboardUseCase<T1, T2>>>,
+    Path(quest_id): Path<i32>,
+    Extension(adventurer_id): Extension<i32>)
+    -> impl IntoResponse
+where
+    T1: CrewSwitchboardRepository + Send + Sync,
+    T2: QuestViewingRepository + Send + Sync,
+{
+    unimplemented!()
+}
+
